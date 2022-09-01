c5bf5223543efa7f5370bbdd629ac8f779dbcf83
Jose Morales <j_morales@csu.fullerton.edu>
2022-09-01 13:35:18 -0700

    files

---
+++ stockstats.py
@@ -0,0 +1,9 @@
+import sys
+import pygame as pg
+
+
+class create_stockstats:
+    def __init__(self, start):
+        self.screen = start.screen
+        self.create_Stockstats_page_finished = False
+
