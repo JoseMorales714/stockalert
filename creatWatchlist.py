e2e178704aa220815a45de8f48ea87909936c2c1
Jose Morales <j_morales@csu.fullerton.edu>
2022-09-01 13:39:10 -0700

    add watchlist

---
+++ creatWatchlist.py
@@ -0,0 +1,10 @@
+import sys
+import pygame as pg
+
+
+class create_Watchlist:
+    def __init__(self, start):
+        self.screen = start.screen
+        self.create_Watchlist_page_finished = False
+
+
