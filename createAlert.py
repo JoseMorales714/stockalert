3a5f3a179fe78e86a5ec5b8e4f3c5eb48bfeef49
juan sierra <sierra.juan99@gmail.com>
2022-09-06 14:37:14 -0700

    new functionality added: A list will be made and saved when user inputs their ticker

--- createAlert.py
+++ createAlert.py
@@ -3,14 +3,21 @@ import pygame as pg
 import pygame.event
 import pygame_gui
 from settings import Settings
+import pickle
 
 # Allow user to input stocks. There will be a 5 stock limit
+
+
 class create_Alert:
     def __init__(self, start):
         self.screen = start.screen
         self.create_Alert_finished = False
         self.alert_list = [] # An empty list will be made to hold the tickers
-
+        try:
+            with open('watchlist.txt', 'rb') as fh:
+                self.alert_list = pickle.load(fh)
+        except FileNotFoundError:
+            print('File not found')
 
     def show(self):
         w = 900
@@ -20,6 +27,11 @@ class create_Alert:
         Manager = pygame_gui.UIManager((w, h))
         input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 10), (80, 40)), manager=Manager,
                                                     object_id="#main_text_entry")
+        input.set_forbidden_characters('numbers')
+        font = pygame.font.Font(None, 32)
+        text = font.render('Enter stock tckr', (0, 0, 0), False)
+        textRect =  text.get_rect()
+        textRect.center = (500, 500)
 
         while True:
             for event in pygame.event.get():
@@ -29,15 +41,12 @@ class create_Alert:
                 if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
                     self.add_list(event.text)
                     print(self.get_list())
-
                 Manager.process_events(event)
             Manager.update(refresh_rate/10000)
             self.screen.fill('blue')
             Manager.draw_ui(self.screen)
+            self.screen.blit(text, textRect)
             pygame.display.update()
-
-
-
 
     # return the tickers the user wants
     def get_list(self):
@@ -47,6 +56,9 @@ class create_Alert:
     def add_list(self, tckr):
         if len(self.alert_list) < 5:
             self.alert_list.append(tckr)
+            with open('watchlist.txt', 'wb') as fh:
+                pickle.dump(self.alert_list, fh)
+                print('file saved')
         else:
             print('capacity has been reached')
 
