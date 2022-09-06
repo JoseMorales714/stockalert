e120bb2537fab2ad4f1faf69f92e35386240e50a
luisvenegas214 <112129491+luisvenegas214@users.noreply.github.com>
2022-09-06 14:29:06 -0700

    Update creatWatchlist.py
    Added title and working on making the back button

--- creatWatchlist.py
+++ creatWatchlist.py
@@ -1,10 +1,67 @@
 import sys
 import pygame as pg
+import pygame.event
+import pygame_gui
+from button import Button
+from startScreen import start_Screen
+
+
+
+
+def get_font(size):
+    return pygame.font.Font(None, size)
 
 
 class create_Watchlist:
     def __init__(self, start):
         self.screen = start.screen
         self.create_Watchlist_page_finished = False
+        self.back_btn_click = False
+
+
+
+
+
+    def show(self):
+        w = 700
+        h = 700
+        CLOCK = pygame.time.Clock()
+        refresh_rate = CLOCK.tick(30)
+        Manager = pygame_gui.UIManager((w, h))
+        input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 10), (80, 40)), manager=Manager,
+
+                                                 object_id="#main_text_entry")
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+                    self.add_list(event.text)
+                    print(self.get_list())
+
+                Manager.process_events(event)
+            Manager.update(refresh_rate/10000)
+            self.screen.fill('black')
+            PLAY_MOUSE_POS = pygame.mouse.get_pos()
+            Manager.draw_ui(self.screen)
+            PLAY_TEXT = get_font(160).render("Watchlist", True, "Green")
+            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
+            self.screen.blit(PLAY_TEXT, PLAY_RECT)
+            self.back_btn = Button(self.screen, "back", ul=(-10,60))
+            self.back_btn.draw()
+
+
+            pygame.display.update()
+
+
+
+
+
+
+
+
+
 
 
