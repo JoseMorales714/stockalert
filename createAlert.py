b4213947e4f6e10f443b59ca0d4d69b0db938731
juan sierra <sierra.juan99@gmail.com>
2022-10-19 15:30:08 -0700

    added to interface

--- createAlert.py
+++ createAlert.py
@@ -7,6 +7,18 @@ from settings import Settings
 import pickle
 
 # Allow user to input stocks. There will be a 5 stock limit
+try:
+    with open('watchlist.txt', 'rb') as fh:
+        alert_list = pickle.load(fh)
+except FileNotFoundError:
+    alert_list = set()
+    print('File watchlist.txt not found ')
+try:
+    with open('email.txt', 'rb') as fh:
+        email_address = pickle.load(fh)
+except FileNotFoundError:
+    email_address = ''
+    print('File email.txt not found ')
 
 
 class create_Alert:
@@ -26,20 +38,34 @@ class create_Alert:
         CLOCK = pygame.time.Clock()
         refresh_rate = CLOCK.tick(30)
         Manager = pygame_gui.UIManager((w, h))
-        input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (150, 40)), manager=Manager,
-                                                    object_id="#main_text_entry")
-        input.set_text('enter ticker here')
-        input.set_allowed_characters(list(map(str, string.ascii_letters)))  # only allow alphabet
-        font = pygame.font.Font(None, 32)
-        # text = font.render('Enter stock ticker:', (0, 0, 0), False)
-        # textRect = text.get_rect()
-        # textRect.center = (105, 70)
+        font = pygame.font.Font(None, 26)
+        text = font.render('Welcome, ' + str(email_address), (0, 0, 0), False)
+        textRect = text.get_rect()
+        textRect = (10, 20)
+        confirmation_button = pygame_gui.elements.UIButton(text='add', manager=Manager,
+                                                           relative_rect=pygame.Rect((10, 100), (100, 50)),
+                                                           object_id='#confirm')
+        confirmation_button.disable()
+        done_button = pygame_gui.elements.UIButton(text='done', manager=Manager,
+                                                   relative_rect=pygame.Rect((110, 100), (100, 50)),
+                                                   object_id='#finished')
+        if email_address != '':  # TO DO: add how to check for valid email
+            print(email_address)
+            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (150, 40)), manager=Manager,
+                                                        object_id="#main_text_entry")
+            input.set_text('enter ticker')
+            input.set_allowed_characters(
+                list(map(str, string.ascii_letters)))  # only allow alphabet characters for input
+        else:
+            print('No user')
+            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (350, 40)), manager=Manager,
+                                                        object_id="#username_text_entry")
+            input.set_text('enter email')
 
-        
         # to send an email, we can have a bool and if its true it will send the email
         # this conditional statemnet for the bool can be inside the while True block
         # however, we still need the user's email so we'll have to get user input in GUI form
-        
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
