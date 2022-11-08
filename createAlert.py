074ea38786e8e6ebab7e723c4e2f7610ebb91b41
juan sierra <sierra.juan99@gmail.com>
2022-11-08 14:33:24 -0800

    changed ui

--- createAlert.py
+++ createAlert.py
@@ -33,42 +33,42 @@ class create_Alert:
     def __init__(self, start):
         self.screen = start.screen
         self.create_Alert_finished = False
-        self.alert_list = []  # An empty list will be made to hold the tickers
+        # self.alert_list = []  # An empty list will be made to hold the tickers
         self.haveUser = False
 
     def show(self):
-        w = 900
-        h = 900
+        w = 1200
+        h = 800
         CLOCK = pygame.time.Clock()
         refresh_rate = CLOCK.tick(30)
-        Manager = pygame_gui.UIManager((w, h))
+        Manager = pygame_gui.UIManager((w, h), 'theme.json')
         font = pygame.font.Font(None, 26)
         text = font.render('Welcome, ' + str(email_address), (0, 0, 0), False)
         textRect = text.get_rect()
         textRect = (10, 20)
         confirmation_button = pygame_gui.elements.UIButton(text='add', manager=Manager,
-                                                           relative_rect=pygame.Rect((10, 100), (100, 50)),
+                                                           relative_rect=pygame.Rect(w/2 - 50, h/2 + 20, 100, 40),
                                                            object_id='#confirm')
         confirmation_button.disable()
         done_button = pygame_gui.elements.UIButton(text='alert', manager=Manager,
-                                                   relative_rect=pygame.Rect((110, 100), (100, 50)),
+                                                   relative_rect=pygame.Rect(w/2 - 50, h/2 + 60, 100, 40),
                                                    object_id='#finished')
         back_button = pygame_gui.elements.UIButton(text='return', manager=Manager,
-                                                   relative_rect=pygame.Rect((110, 150), (100, 50)),
+                                                   relative_rect=pygame.Rect(35, h - 160, 100, 50),
                                                    object_id='#back')
         if email_address != '':
             changeEmail_button = pygame_gui.elements.UIButton(text='change email', manager=Manager,
-                                                              relative_rect=pygame.Rect((610, 100), (150, 50)),
+                                                              relative_rect=pygame.Rect(w - 180, 30, 150, 50),
                                                               object_id='#change')
             print(email_address)
-            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (150, 40)), manager=Manager,
+            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(w/2 - 150/2, h/2 - 40/2, 150, 40), manager=Manager,
                                                         object_id="#main_text_entry")
-            input.set_text('enter ticker')
+            input.set_text('Enter ticker')
             input.set_allowed_characters(
                 list(map(str, string.ascii_letters)))  # only allow alphabet characters for input
         else:
             print('No user')
-            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (350, 40)), manager=Manager,
+            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(w/2 - 350/2, h/2 - 40/2, 350, 40), manager=Manager,
                                                         object_id="#username_text_entry")
             input.set_text('enter email')
             input.set_forbidden_characters([' '])  # no spaces allowed
@@ -100,26 +100,25 @@ class create_Alert:
                         open('watchlist.txt', 'w').close()
                         changeEmail_button.kill()
                         input.kill()
-                        input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (350, 40)),
+                        input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((w/2 - 350/2, h/2 - 40/2), (350, 40)),
                                                                     manager=Manager,
                                                                     object_id="#username_text_entry")
                         input.set_text('enter email')
-
                 if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#username_text_entry":
                     self.get_user(event.text)
                     print(self.get_list())
                     print(email_address)
                     input.kill()
-                    input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((10, 50), (150, 40)),
+                    input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((w/2 - 150/2, h/2 - 40/2), (150, 40)),
                                                                 manager=Manager,
                                                                 object_id="#main_text_entry")
                     changeEmail_button = pygame_gui.elements.UIButton(text='change email', manager=Manager,
-                                                                      relative_rect=pygame.Rect((610, 100), (150, 50)),
+                                                                      relative_rect=pygame.Rect((w - 180, 30), (150, 50)),
                                                                       object_id='#change')
-                    input.set_text('enter ticker')
+                    input.set_text('Enter ticker')
                 Manager.process_events(event)
             Manager.update(refresh_rate / 10000)
-            self.screen.fill('blue')
+            self.screen.fill((237, 184, 121))
             Manager.draw_ui(self.screen)
             self.screen.blit(text, textRect)
             pygame.display.update()
