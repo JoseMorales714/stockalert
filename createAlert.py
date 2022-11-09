ddf60acaf4b77a85d362082979f8f2f63f47bfe6
juan sierra <sierra.juan99@gmail.com>
2022-11-08 23:08:48 -0800

    ui change and code clean up

--- createAlert.py
+++ createAlert.py
@@ -6,7 +6,6 @@ import pygame_gui
 from mail import create_email
 from settings import Settings
 import pickle
-from button import Button
 import yfinance as yf
 
 try:
@@ -28,7 +27,6 @@ except EOFError:
     email_address = ''
     print("No input to read")
 
-
 class create_Alert:
     def __init__(self, start):
         self.screen = start.screen
@@ -43,17 +41,16 @@ class create_Alert:
         refresh_rate = CLOCK.tick(30)
         Manager = pygame_gui.UIManager((w, h), 'theme.json')
         font = pygame.font.Font(None, 26)
-        text = font.render('Welcome, ' + str(email_address), (0, 0, 0), False)
+        text = font.render('Welcome, ' + str(email_address), (255, 255, 255), False)
         textRect = text.get_rect()
-        textRect = (10, 20)
-        confirmation_button = pygame_gui.elements.UIButton(text='add', manager=Manager,
+        add_ticker_button = pygame_gui.elements.UIButton(text='Add', manager=Manager,
                                                            relative_rect=pygame.Rect(w/2 - 50, h/2 + 20, 100, 40),
                                                            object_id='#confirm')
-        confirmation_button.disable()
-        done_button = pygame_gui.elements.UIButton(text='alert', manager=Manager,
+        add_ticker_button.disable()
+        pygame_gui.elements.UIButton(text='Alert', manager=Manager,
                                                    relative_rect=pygame.Rect(w/2 - 50, h/2 + 60, 100, 40),
                                                    object_id='#finished')
-        back_button = pygame_gui.elements.UIButton(text='return', manager=Manager,
+        pygame_gui.elements.UIButton(text='Return', manager=Manager,
                                                    relative_rect=pygame.Rect(35, h - 160, 100, 50),
                                                    object_id='#back')
         if email_address != '':
@@ -61,17 +58,10 @@ class create_Alert:
                                                               relative_rect=pygame.Rect(w - 180, 30, 150, 50),
                                                               object_id='#change')
             print(email_address)
-            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(w/2 - 150/2, h/2 - 40/2, 150, 40), manager=Manager,
-                                                        object_id="#main_text_entry")
-            input.set_text('Enter ticker')
-            input.set_allowed_characters(
-                list(map(str, string.ascii_letters)))  # only allow alphabet characters for input
+            input = make_ticker_input(w, h, Manager)
         else:
             print('No user')
-            input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(w/2 - 350/2, h/2 - 40/2, 350, 40), manager=Manager,
-                                                        object_id="#username_text_entry")
-            input.set_text('enter email')
-            input.set_forbidden_characters([' '])  # no spaces allowed
+            input = make_email_input(w, h, Manager)
 
         while not self.create_Alert_finished:
             for event in pygame.event.get():
@@ -79,17 +69,24 @@ class create_Alert:
                     pygame.quit()
                     exit()
                 if event.type == pygame_gui.UI_TEXT_ENTRY_CHANGED and (event.ui_object_id == "#main_text_entry"):
-                    confirmation_button.set_text('Add')
-                    confirmation_button.enable()
+                    add_ticker_button.enable()
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_object_id == '#confirm':
                         ticker = yf.Ticker(input.text)
                         if (ticker.info['regularMarketPrice'] == None):
                             raise NameError("You did not input a correct stock ticker! Try again.")
-                        self.add_list(input.text)
-                        print(self.get_list())
-                        input.set_text('')
-                        confirmation_button.disable()
+                        else:  #  valid ticker, add to the list
+                            print(input.text)
+                            if len(alert_list) < 5:
+                                alert_list.add(str.upper(input.text))
+                                with open('watchlist.txt', 'wb') as fh:
+                                    pickle.dump(alert_list, fh)
+                                    print('watchlist File saved')
+                            else:
+                                print('capacity has been reached')
+                            print(self.get_list())
+                            input.set_text('')
+                            add_ticker_button.disable()
                     if event.ui_object_id == '#finished':
                         alert = create_email()
                         alert.send_email(user_email=email_address, stocks=alert_list)
@@ -100,25 +97,20 @@ class create_Alert:
                         open('watchlist.txt', 'w').close()
                         changeEmail_button.kill()
                         input.kill()
-                        input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((w/2 - 350/2, h/2 - 40/2), (350, 40)),
-                                                                    manager=Manager,
-                                                                    object_id="#username_text_entry")
-                        input.set_text('enter email')
+                        input = make_email_input(w, h, Manager)
                 if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#username_text_entry":
                     self.get_user(event.text)
                     print(self.get_list())
                     print(email_address)
                     input.kill()
-                    input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((w/2 - 150/2, h/2 - 40/2), (150, 40)),
-                                                                manager=Manager,
-                                                                object_id="#main_text_entry")
+                    input = make_ticker_input(w, h, Manager)
                     changeEmail_button = pygame_gui.elements.UIButton(text='change email', manager=Manager,
                                                                       relative_rect=pygame.Rect((w - 180, 30), (150, 50)),
                                                                       object_id='#change')
                     input.set_text('Enter ticker')
                 Manager.process_events(event)
             Manager.update(refresh_rate / 10000)
-            self.screen.fill((237, 184, 121))
+            self.screen.fill((35, 41, 49))
             Manager.draw_ui(self.screen)
             self.screen.blit(text, textRect)
             pygame.display.update()
@@ -127,23 +119,30 @@ class create_Alert:
     def get_list(self):
         return alert_list
 
-    # add a ticker to the list, with a limit of 5
-    def add_list(self, tckr):
-        print(tckr)
-        if len(alert_list) < 5:
-            alert_list.add(str.upper((tckr)))
-            with open('watchlist.txt', 'wb') as fh:
-                pickle.dump(alert_list, fh)
-                print('watchlist File saved')
-        else:
-            print('capacity has been reached')
-
     def get_user(self, username):
         self.haveUser = True
         with open('email.txt', 'wb') as fh:
             pickle.dump(username, fh)
             print('user file saved')
 
+        
+def make_ticker_input(xpos, ypos, manager):
+    ticker_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(xpos / 2 - 150 / 2, ypos / 2 - 40 / 2, 150, 40),
+                                        manager=manager,
+                                        object_id="#main_text_entry")
+    ticker_input.set_text('Enter ticker')
+    ticker_input.set_allowed_characters(
+        list(map(str, string.ascii_letters)))  # only allow alphabet characters for input
+    return ticker_input
+
+
+def make_email_input(xpos, ypos, manager):
+    email_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(xpos / 2 - 350 / 2, ypos / 2 - 40 / 2, 350, 40),
+                                                manager=manager,
+                                                object_id="#username_text_entry")
+    email_input.set_text('enter email')
+    email_input.set_forbidden_characters([' '])  # no spaces allowed
+    return email_input
 
 
 
