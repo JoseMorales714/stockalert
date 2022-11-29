0214a6207e15f536793c27be4304aaf26c2eb348
luisvenegas214 <112129491+luisvenegas214@users.noreply.github.com>
2022-11-29 14:03:49 -0800

    Update creatWatchlist.py
    UPDATED

--- creatWatchlist.py
+++ creatWatchlist.py
@@ -8,8 +8,6 @@ from createAlert import alert_list
 from startScreen import start_Screen
 import yfinance as yf
 
-BLACK = (0, 0, 0)
-
 
 def get_font(size):
     return pygame.font.Font(None, size)
@@ -19,57 +17,72 @@ class create_Watchlist():
     def __init__(self, start):
         self.screen = start.screen
         self.create_Watchlist_page_finished = False
-        self.back_btn_click = False
-        self.back_btn = Button(self.screen, "back", ul=(-10, 60))
 
-    def mouse_on_back(self):
-        mouse_x, mouse_y = pg.mouse.get_pos()
-        return self.back_btn.rect.collidepoint(mouse_x, mouse_y)
+    def show(self):
+        bg = pg.image.load("createwatchlist.png")
+        bg = pg.transform.scale(bg,(1200, 600))
+        w = 1200
+        h = 800
+        CLOCK = pygame.time.Clock()
+        PLAY_TEXT = get_font(120).render("Watchlist", True, "Green")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(600, 100))
+        refresh_rate = CLOCK.tick(30)
+        Manager = pygame_gui.UIManager((w, h), 'theme.json')
+        click = pg.mixer.Sound("mixkit-classic-click-1117.wav") #adds sound
 
-    def check_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-                exit()
-            if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
-                self.add_list(event.text)
-                print(self.get_list())
-            if event.type == pg.MOUSEBUTTONDOWN:
-                if self.mouse_on_back():
-                    self.back_btn_click = True
-                    self.create_Watchlist_page_finished = True
 
-    def draw(self):
-        display_list = []  # will contain the renders for each tckr
-        for i in alert_list:  # for every tckr
-            ticker = yf.Ticker(str(i))
-            price = str(ticker.info['regularMarketPrice'])
-            PLAY_TEXT = get_font(90).render('$' + str(i) + ' ' + price, True, "Green")  # create a render
-            display_list.append(PLAY_TEXT)  # append it to display_list
-        self.screen.fill(BLACK)
-        self.back_btn.draw()
-        pygame.display.set_caption("Watchlist")
-        # Icon = pygame.image.load("watchlist.svg.png")
-        # pygame.display.set_icon(Icon)
-        # PLAY_TEXT = get_font(120).render("Watchlist", True, "Green")
-        # PLAY_RECT = PLAY_TEXT.get_rect(center=(500, 100))
-        # self.screen.blit(PLAY_TEXT, PLAY_RECT)
-        # PLAY_TEXT = get_font(80).render("apple", True, "red")   # just and idea how it will look
-        # PLAY_RECT = PLAY_TEXT.get_rect(center=(200, 180))
-        # self.screen.blit(PLAY_TEXT, PLAY_RECT)
-        # PLAY_TEXT = get_font(80).render("120", True, "green")  # price
-        # PLAY_RECT = PLAY_TEXT.get_rect(center=(350, 180))
-        # self.screen.blit(PLAY_TEXT, PLAY_RECT)
-        y = 100
-        for d in display_list:  # for every render in display_list
-            self.screen.blit(d, PLAY_TEXT.get_rect(center=(640, y)))  # blit
-            y += 50  # so that the next rect is lower on the screen
-        pg.display.flip()
 
-    def show_Watchlist(self):
+       #BUTTONS
+        pygame_gui.elements.UIButton(text='Back', manager=Manager, relative_rect=pygame.Rect(35, 10, 100, 50),
+                                     object_id='#back')
+        pygame_gui.elements.UIButton(text='Clear', manager=Manager, relative_rect=pygame.Rect(1000,520,100,50),
+                                     object_id='clear')
+
+
         while not self.create_Watchlist_page_finished:
-            self.draw()
-            self.check_events()
+            display_list = []  # will contain the renders for each tckr
+            #for i in alert_list:  # for every tckr
 
+                #pg.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+                    self.add_list(event.text)
+                    print(self.get_list())
+                if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_object_id == '#back':
+                      pg.mixer.Sound.play(click)
+                      pg.mixer.music.stop()
+                      self.create_Watchlist_page_finished = True
+                    if event.ui_object_id == 'clear':
+                        pg.mixer.Sound.play(click)
+                        pg.mixer.music.stop()
+                        open('watchlist.txt','w').close()
+
+                Manager.process_events(event)
+            Manager.update(refresh_rate / 10000)
+            self.screen.blit(bg, (0,0))
+            Manager.draw_ui(self.screen)
+            pygame.display.set_caption("Watchlist")
+            Icon = pygame.image.load("watchlist.svg.png")
+            pygame.display.set_icon(Icon)
+            self.screen.blit(PLAY_TEXT, PLAY_RECT)
+            for i in alert_list:
+                ticker = yf.Ticker(str(i))
+                price = str(ticker.info['regularMarketPrice'])
+                PLAY_TEXT1 = get_font(90).render('$' + str(i) + ' ' + price, True, "White")  # create a render
+                display_list.append(PLAY_TEXT1)
+            y = 200
+            for d in display_list:  # for every render in display_list
+                self.screen.blit(d, PLAY_TEXT1.get_rect(center=(600, y)))  # blit
+                y += 100  # so that the next rect is lower on the screen
             pygame.display.update()
+
+
+
+
+
+
+
